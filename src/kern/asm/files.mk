986dc96960ad9ff64f9c419f23729c3ca90f2532
Johannes Krottmayer <github.krotti42@proton.me>
2025-06-13 20:27:03 +0200

    src/kern/asm: add local Makefile
    Add initial local Makefile.
    
    FILES:
    A  src/kern/asm/files.mk
    
    Signed-off-by: Johannes Krottmayer <github.krotti42@proton.me>

---
+++ src/kern/asm/files.mk
@@ -0,0 +1,13 @@
+#
+# QEMU Cortex-A53 (ARMv8-A) minimal bare-metal example
+#
+
+KERN_DIR			= ./asm
+
+KERN_ASRC			+= $(KERN_DIR)/locore.S
+KERN_ASRC			+= $(KERN_DIR)/mmu.S
+KERN_ASRC			+= $(KERN_DIR)/start.S
+KERN_ASRC			+= $(KERN_DIR)/crt0.S
+KERN_ASRC			+= $(KERN_DIR)/vector.S
+KERN_ASRC			+= $(KERN_DIR)/user.S
+KERN_ASRC			+= $(KERN_DIR)/intr.S
