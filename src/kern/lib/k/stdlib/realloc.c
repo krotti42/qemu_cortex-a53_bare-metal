142b70704350a693195d9e52da76d651f148650e
Johannes Krottmayer <github.krotti42@proton.me>
2025-06-14 21:09:19 +0200

    src/kern/lib/k/stdlib: add kernel library memory allocator functions
    Add kernel library higher memory allocator functions.
    
    TODO:
    
    - Add new function for aligned allocation.
    
    FILES:
    A  src/kern/lib/k/stdlib/calloc.c
    A  src/kern/lib/k/stdlib/free.c
    A  src/kern/lib/k/stdlib/malloc.c
    A  src/kern/lib/k/stdlib/realloc.c
    
    Signed-off-by: Johannes Krottmayer <github.krotti42@proton.me>

---
+++ src/kern/lib/k/stdlib/realloc.c
@@ -0,0 +1,50 @@
+/**
+ *
+ * QEMU Cortex-A53 (ARMv8-A) minimal bare-metal example
+ *
+ * Copyright (c) 2025 Johannes Krottmayer <github.krotti42@proton.me>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <k/stdlib.h>
+#include <k/string.h>
+
+#include <_libk.h>
+
+void *realloc(void *ptr, size_t size)
+{
+    size_t old_size;
+    void *p;
+    
+    if (!ptr)
+        return NULL;
+    
+    if (!size)
+        return NULL;
+    
+    old_size = _libk_mem_size(ptr);
+    
+    if (old_size < 1)
+        return NULL;
+    
+    p = _libk_mem_alloc(size);
+    
+    if (!p)
+        return NULL;
+    
+    memmove(p, ptr, old_size);
+    
+    return p;
+}
