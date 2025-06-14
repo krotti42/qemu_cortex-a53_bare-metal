06936ec2eab065ca6591cefb48867407ae292722
Johannes Krottmayer <github.krotti42@proton.me>
2025-06-14 22:39:50 +0200

    src/kern/asm/files.mk: add libk local Makefile
    Add libk local Makefile.
    
    FILES:
    M  src/kern/asm/files.mk
    
    Signed-off-by: Johannes Krottmayer <github.krotti42@proton.me>

--- src/kern/asm/files.mk
+++ src/kern/asm/files.mk
@@ -2,13 +2,15 @@
 # QEMU Cortex-A53 (ARMv8-A) minimal bare-metal example
 #
 
-KERN_DIR			= ./asm
+KERN_ASM_DIR			= ./asm
 
-KERN_ASRC			+= $(KERN_DIR)/locore.S
-KERN_ASRC			+= $(KERN_DIR)/mmu.S
-KERN_ASRC			+= $(KERN_DIR)/start.S
-KERN_ASRC			+= $(KERN_DIR)/cache.S
-KERN_ASRC			+= $(KERN_DIR)/crt0.S
-KERN_ASRC			+= $(KERN_DIR)/vector.S
-KERN_ASRC			+= $(KERN_DIR)/user.S
-KERN_ASRC			+= $(KERN_DIR)/intr.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/locore.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/mmu.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/start.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/cache.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/crt0.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/vector.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/user.S
+KERN_ASRC			+= $(KERN_ASM_DIR)/intr.S
+
+include $(KERN_ASM_DIR)/libk/files.mk
